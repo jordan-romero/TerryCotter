08c551192dd6b309ab7487fe7314ceb8c0b48c35
jordles113 <jordan.t.romero@gmail.com>
2021-01-20 15:58:49 -0600

    creates a model and connects it to the controller

--- Terry Cotter and the Enchanter's Pebble/Controller/ViewController.swift
+++ Terry Cotter and the Enchanter's Pebble/Controller/ViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    
+    let stories = [
+        Story(title: "Welcome Terry Cotter",
+              choice1: "Join Luffelpoof",
+              choice2: "Join RrifanClaw"
+        ),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."
+        ),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps."
+        ),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        storyLabel.text = stories[0].title
+        choice1Button.setTitle(stories[0].choice1, for: .normal)
+        choice2Button.setTitle(stories[0].choice2, for: .normal)
     }
 
 
---
+++ Terry Cotter and the Enchanter's Pebble/Model/Story.swift
@@ -0,0 +1,15 @@
+//
+//  Story.swift
+//  Terry Cotter and the Enchanter's Pebble
+//
+//  Created by Jordan Romero on 1/20/21.
+//
+
+import Foundation
+
+struct Story {
+    var title: String
+    var choice1: String
+    var choice2: String
+    
+}
