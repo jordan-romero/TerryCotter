436b3e4c3a2bf1b5a55b58295567694c53564374
jordles113 <jordan.t.romero@gmail.com>
2021-01-22 16:15:50 -0600

    adds more stories, still have UILabel issues as the text goes off the screen

--- Terry Cotter and the Enchanter's Pebble/Model/Story.swift
+++ Terry Cotter and the Enchanter's Pebble/Model/Story.swift
@@ -10,6 +10,7 @@ import Foundation
 struct Story {
     var title: String
     var choice1: String
+    var choice1Destination: Int
     var choice2: String
-    
+    var choice2Destination: Int
 }
